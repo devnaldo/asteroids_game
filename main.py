818027a79b92541fe9ba86870cfd0a81727c13f8
devnaldo <188571010+devnaldo@users.noreply.github.com>
2026-09-15 14:42:11 +0000

    added player movement

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     pygame.init()   
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dlt = 0.0
+    dt = 0.0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True: 
         log_state()
@@ -16,14 +16,18 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dlt = clock.tick(60) / 1000 # limit the framerate to 60 FPS
+        
 
+        player.update(dt)
         screen.fill("black") 
         player.draw(screen)
         pygame.display.flip()
-        
+        dt = clock.tick(60) / 1000 # limit the framerate to 60 FPS
 
    
 
 if __name__ == "__main__":
     main()
+
+
+                                        
